57ec78d841f44699a3ae03db9eef99ce53ffcd78
joshua <lionel1265@gmail.com>
2023-12-08 17:21:36 +0900

    [chore] fix typo

--- scripts/citeseer_experiements.sh
+++ scripts/citeseer_experiements.sh
@@ -3,11 +3,11 @@
 source C:/Users/Joshua/anaconda3/Scripts/activate py38
 
 # DeepWalk
-python C:/Repositories/node2vec/src/pagerank_node2vec.py citeseer deepwalk 5 cora_deepwalk
+python C:/Repositories/node2vec/src/pagerank_node2vec.py citeseer deepwalk 5 citeseer_deepwalk
 
 # Node2Vec
-python C:/Repositories/node2vec/src/pagerank_node2vec.py citeseer node2vec 5 cora_node2vec
+python C:/Repositories/node2vec/src/pagerank_node2vec.py citeseer node2vec 5 citeseer_node2vec
 
 # PageRank with Node2Vec
-python C:/Repositories/node2vec/src/pagerank_node2vec.py citeseer pagerank 5 cora_pagerank
+python C:/Repositories/node2vec/src/pagerank_node2vec.py citeseer pagerank 5 citeseer_pagerank
 
